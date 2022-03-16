83ad41828f36bd79e76288a66da1bb6043c80134
agurishimabukuro <aguri27@hotmail.com>
2022-03-16 17:56:17 -0300

    Cambio2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className='main-header'>
         <img src="logo192.png" className="img-header" alt="logo"></img>
+        <ul className='navbar'>
+          <li><button>Home</button></li>
+          <li><button>Productos</button></li>
+          <li><button>Nosotros</button></li>
+          <li><button>Contacto</button></li>
+        </ul>
       </header>
     </div>
   );
