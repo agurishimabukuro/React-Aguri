523980acb46124e9a82719f5d721e76e54f4bd47
agurishimabukuro <aguri27@hotmail.com>
2022-03-24 16:53:20 -0300

    cambio 24-3

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,13 +7,13 @@ function NavBar() {
     return(
         <header className='main-header'>
             <div className="container-logo">
-            <img src="inter.png" alt="logo"></img>
+                <img src='react-aguri\public\inter.png' alt="logo"></img>
             </div>
             <ul className='navbar'>
                 <li><Button variant="contained">Home</Button></li>
-                <li><Button variant="outlined">Nosotros</Button></li>
+                <li><Button variant="contained">Nosotros</Button></li>
                 <li><Button variant="contained">Productos</Button></li>
-                <li><Button variant="outlined">Contacto</Button></li>
+                <li><Button variant="contained">Contacto</Button></li>
             </ul>
             <Button><CartWidget/></Button>
         </header>
