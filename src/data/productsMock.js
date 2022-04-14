e7a1e3240e89b85b8ee304ec00986f0d8f09c2da
agurishimabukuro <aguri27@hotmail.com>
2022-04-14 13:58:27 -0300

    cambio precio

--- src/data/productsMock.js
+++ src/data/productsMock.js
@@ -1,7 +1,7 @@
 const mockProductos = [
     {
       title: 'Fernet',
-      price: 540,
+      price: 860,
       size: '750ml',
       stock: 12,
       id: 1,
@@ -11,7 +11,7 @@ const mockProductos = [
     },
     {
       title: 'Vino',
-      price: 260,
+      price: 530,
       size: '500ml',
       stock: 2,
       id: 2,
@@ -21,7 +21,7 @@ const mockProductos = [
     },
     {
       title: 'Cerveza',
-      price: 125,
+      price: 210,
       size: '700ml',
       stock: 6,
       id: 3,
@@ -31,7 +31,7 @@ const mockProductos = [
     },  
     {
       title: 'Gaseosa',
-      price: 150,
+      price: 225,
       size: '2.5lts',
       stock: 22,
       id: 4,
